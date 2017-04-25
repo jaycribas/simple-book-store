01161cc0d5fc25b8d7bc6e096cd99ad5e6f42241
smithrm941 <smithrm941@gmail.com>
2017-04-25 16:20:44 -0700

    search title, search author, search genre working

--- simple-book-store/routes/index.js
+++ simple-book-store/routes/index.js
@@ -13,7 +13,36 @@ router.get('/', (req, res, next) => {
   })
 });
 
-//GET
+//GET title
+router.get('/title/:title', (req, res) => {
+  const title = req.params.title
+  console.log(title)
+  db.searchTitles(title)
+  .then(books => {
+    res.render('index', { books: books})
+  } )
+})
+
+//GET author
+router.get('/author/:author', (req, res) => {
+  const author = req.params.author
+  console.log(author)
+  db.searchAuthors(author)
+  .then(books => {
+    res.render('index', { books: books})
+  } )
+})
+
+//GET genre
+router.get('/genre/:genre', (req, res) => {
+  const genre = req.params.genre
+  console.log(genre)
+  db.searchGenres(genre)
+  .then(books => {
+    res.render('index', { books: books})
+  } )
+})
+
 //POST
 
 router.post('/addbook', (req, res) => {
@@ -36,6 +65,26 @@ router.post('/addbook', (req, res) => {
 })
 
 //PUT
+router.put('/editbook/:id', (req, res) => {
+  let book = req.body
+  const id = req.params.id
+  console.log(book)
+  db.editBook(id, book)
+  .then(() =>
+  res.status(200)
+    .json({
+      status: 'success',
+      message: 'Edited book'
+    }))
+  .catch(error => {
+    res.status(500).render('error', {
+      error: error,
+      message: error.message,
+    })
+  })
+})
+
+
 //DELETE
 
 router.delete('/deletebook/:id', (req, res) => {
